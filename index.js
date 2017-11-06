9125799d776c2691016e1a2b3a250b49c083cafe
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-11-06 17:43:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ function deleteFromObjectByKey(object,key){
   delete tempObject[key]
   return tempObject
 }
+function destructivelyDeleteFromObjectByKey(object,key){
+  delete object[key]
+  return object
+}
